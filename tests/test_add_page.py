f347bd9c085a969fce6ed65f2fa61d9f1da0b398
Lucas Cimon <925560+Lucas-C@users.noreply.github.com>
2021-04-01 15:38:29 +0200

    Pleasing pep8speaks

--- tests/test_add_page.py
+++ tests/test_add_page.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
-import hashlib, os
+import hashlib
+import os
 
 from pdfrw import PdfReader, PdfWriter
 from pdfrw.objects import PdfName, PdfDict, IndirectPdfDict
@@ -15,7 +16,8 @@ import static_pdfs
 class TestAddPage(unittest.TestCase):
 
     def test_append_page(self):
-        in_filepath = static_pdfs.pdffiles[0][5]  # global/0ae80b493bc21e6de99f2ff6bbb8bc2c.pdf
+        # global/0ae80b493bc21e6de99f2ff6bbb8bc2c.pdf
+        in_filepath = static_pdfs.pdffiles[0][5]
         out_filepath = "test_append_page.pdf"
         for new_page_index, expected_hash in (
             (0, "d76f5573918cba2070da93a10c19d062"),
@@ -28,6 +30,7 @@ class TestAddPage(unittest.TestCase):
             writer.write(out_filepath)
             self.assertEqual(file_hash(out_filepath), expected_hash)
         os.remove(out_filepath)
+
 
 def new_page():
     contents = IndirectPdfDict()
@@ -49,6 +52,7 @@ BT 141.73 700.16 Td (Hello!) Tj ET"""
         ),
         Contents=contents,
     )
+
 
 def file_hash(file_path):
     with open(file_path, 'rb') as data:
